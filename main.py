ed0c7d9c59c118bea4319aa365d95783dff6b539
JinoRino <adg0609@naver.com>
2021-08-26 16:39:07 +0900

    그리디-큰 수의 법칙

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+n, m, k = list(map(int, input().split()))
+data = list(map(int, input().split()))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# 첫번째, 두번째 큰값 -> sort() 후 뒤에서 찾기
+# Max로 찾으려고 했음. -> 중복 값(1,3,3,2)이 나오면 해당 문제 실패
+data.sort()
+fMax = data[n - 1]
+sMax = data[n - 2]
 
+result = 0
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+while True:
+    # range 잘 살펴보기
+    for i in range(k):
+        if m == 0:
+            break
+        result += fMax #k번동안 제일 큰수
+        m -= 1
+    if m == 0:
+        break
+    result += sMax # k번 이후 두번째 큰수
+    m -= 1
+print(result)
